be98ad8a5bb2026d2a3855839220728d018aae75
met5678 <andrew@luminal.io>
2016-07-04 17:13:15 -0400

    Initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,53 @@
+module.exports = function (grunt) {
+   grunt.initConfig({
+      browserify: {
+         dist: {
+            options: {
+               transform: [
+                   ["babelify", { presets: "es2015" } ],
+                   ["require-globify"]
+               ],
+               alias: {
+                  //"alias": "./path"
+               }
+            },
+            files: {
+               "./dist/js/bundle.js": ["./src/app.js"]
+            }
+         }
+      },
+      watch: {
+         options: {
+            livereload: true
+         },
+         gruntfile: {
+            files: ["./Gruntfile.js"],
+            tasks: ["build"]
+         },
+         scripts: {
+            files: ["./src/js/**", "./Gruntfile.js"],
+            tasks: ["compile"]
+         },
+         static: {
+            files: ["./src/{css,pages}/**"],
+            tasks: ["copy"]
+         }
+      },
+      copy: {
+         all: {
+            files: {
+               "./dist/index.html": ["./src/pages/index.html"],
+               "./dist/css/main.css"  : ["./src/css/main.css"]
+            }
+         }
+      }
+   });
+
+   grunt.loadNpmTasks("grunt-browserify");
+   grunt.loadNpmTasks("grunt-contrib-watch");
+   grunt.loadNpmTasks("grunt-contrib-copy");
+
+   grunt.registerTask("default", ["build", "watch"]);
+   grunt.registerTask("compile", ["browserify"]);
+   grunt.registerTask("build", ["compile", "copy"]);
+};
